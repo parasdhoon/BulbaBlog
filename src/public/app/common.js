1695fbeefbf1f449108c6beae87473818e9f85bb
parasdhoon <parasdhoon04@gmail.com>
2024-08-04 23:19:16 +0530

    displaying blogs function added
    Signed-off-by: parasdhoon <parasdhoon04@gmail.com>

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -2,6 +2,7 @@ $(() => {
 
     $('#navbar').load('/components/navbar.html', loginIfNeeded)
     $('#footer').load('/components/footer.html')
+    $('#content').load('/components/all-blogs.html', loadBlogs)
 
 })
 
@@ -16,4 +17,30 @@ function loginIfNeeded() {
     }
     $('#nav-username').text(currentUser.username)
     
+}
+
+function loadBlogs() {
+
+    $.get('/api/blogs', (blogs) => {
+        for(let blog of blogs) {
+            $('#blogs-container').append(
+                $(`
+                <div class="col-4 m-2">
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">${blog.title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${blog.user.username}</h6>
+                        <p class="card-text">
+                            ${blog.body.substr(0, 200)}
+                            <a href="#">...read more
+                        </p>
+                        <a href="#" class="card-link">Comment</a>
+                        <a href="#" class="card-link">Like</a>
+                        </div>
+                    </div>
+                </div>    
+                `)
+            )
+        }
+    })
 }
