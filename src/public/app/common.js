44a57a2190ec0f7aca3d71cf1d7f5937d7cc3f64
parasdhoon <parasdhoon04@gmail.com>
2024-08-04 22:45:16 +0530

    guest user login implemented
    Signed-off-by: parasdhoon <parasdhoon04@gmail.com>

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -1,4 +1,19 @@
 $(() => {
 
-    $('#navbar').load('./navbar.html')
+    $('#navbar').load('/components/navbar.html', loginIfNeeded)
+    $('#footer').load('/components/footer.html')
+
 })
+
+function loginIfNeeded() {
+
+    window.currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if(!currentUser) {
+        $.post('/api/users', {}, (user) => {
+            window.localStorage.user = JSON.stringify(user)
+            currentUser = user
+        })
+    }
+    $('#nav-username').text(currentUser.username)
+    
+}
